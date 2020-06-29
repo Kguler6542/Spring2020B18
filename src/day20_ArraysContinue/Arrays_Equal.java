3e20bd105bf95d0baa824c02bfe42ec9008d6af0
Kguler6542 <Kamuranguler2017@gmail.com>
2020-06-29 15:39:48 -0400

    commit

--- src/day20_ArraysContinue/Arrays_Equal.java
+++ src/day20_ArraysContinue/Arrays_Equal.java
@@ -1,41 +1,28 @@
 package day20_ArraysContinue;
-
 import java.util.Arrays;
 
 public class Arrays_Equal {
 
     public static void main(String[] args) {
 
-        int[] arr1 = {1, 2, 3};
-        int[] arr2 = {1, 2, 3};
+        // Arrays.equals(array1, array2) method: compares two arrays to each other and returns a boolean expression
+        // since .equals(,) returns boolean, can we assign it to a boolean variable? yes we can
 
-        boolean result1 = Arrays.equals(arr1, arr2);
+        int[] arr1 = {1,2,3};
+        int[] arr2 = {1,2,3};
 
-        System.out.println(result1);
+        boolean result1 = Arrays.equals(arr1 , arr2);
+        System.out.println(result1);    // true
 
-        int[] arr3 = {3, 2, 1};
-        int[] arr4 = {1, 2, 3};
-        boolean result2 = Arrays.equals(arr3, arr4);
-        System.out.println(result2);
+        int[] arr3 = {1,3,2};
+        int[] arr4 = {3,2,1};
+        boolean r2= Arrays.equals(arr3,arr4);
+        System.out.println(r2); // false because different element at each index
 
-
-        int[] arr5 = {3, 2, 1};
-        int[] arr6 = {2, 1, 3};
-        Arrays.sort(arr5);  //[ 1,  2,  3]
-        Arrays.sort(arr6); // [ 1, 2, 3]
-
-        boolean result3 = Arrays.equals(arr5, arr6);  // true
-
-        System.out.println(result3);
-
-        int[] array1 = {1,2,3};
-        int[] array2 = {1,2,3,4};
-
-        boolean result4 = Arrays.equals(array1, array2);  // false
-        System.out.println(result4);
-
-
+        Arrays.sort(arr3);      // Arrays.sort() method sorts array in ascending order
+        Arrays.sort(arr4);      // Arrays.sort() method sorts array in ascending order
+        boolean r3 = Arrays.equals(arr3,arr4);
+        System.out.println(r3); //true after they are both sorted in ascending order
 
     }
-
 }
---
+++ src/day54_Maps/Class_Notes.java
@@ -0,0 +1,7 @@
+package day54_Maps;
+
+public class Class_Notes {
+
+
+
+}
